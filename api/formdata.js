e7db186d22ab9a42d863a876768fd5f46b3c138d
arun sharma <arunsharmamoh@mgmail.com>
2021-05-02 16:18:29 +0530

    sdfsdf

--- api/formdata.js
+++ api/formdata.js
@@ -27,101 +27,39 @@ if(name == '' || email=='' || number == '' || company == '' || address=='' || qu
     res.send("please fill all the details")
 }
 else{
+
+    const nodemailer = require('nodemailer');
+  
+  
+    let mailTransporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'arunsharmamoh@gmail.com',
+            pass: 'Arun5207@'
+        }
+    });
+      
+    let mailDetails = {
+        from: '<arunsharmamoh@gmail.com>',
+        to: email,
+        subject: 'Test mail',
+        text: 'Node.js testing mail for GeeksforGeeks'
+    };
+      
+    mailTransporter.sendMail(mailDetails, function(err, data) {
+        if(err) {
+            console.log('Error Occurs');
+        } else {
+            console.log('Email sent successfully');
+            res.send("both mail send");
     
-var nodemailer = require('nodemailer');
- 
-// create reusable transporter object using the default SMTP transport
-
- transporter = nodemailer.createTransport({
-    service:'gmail',
-    auth:{
-        user:'arunsharmamoh@gmail.com',
-        pass:'varunarun181'
-    }
-});
- 
-// setup e-mail data with unicode symbols
-mailOptions = {
-    from: '"Pre-booking form  👻" <arunsharmamoh@gmail.com>', // sender address
-    to: 'arunsharmamoh@gmail.com', // list of receivers
-    subject: 'Pre-bookign form filled', // Subject line
-    text: 'Hello world 🐴', // plaintext body
-    html: "<div> <h1><b>Hello amit bhatti </h1> ,<br> <h3>pre-booking form is filled by someone </h3> <h3> below are the details </h3>🐴</b> <br> <h4> name:"+ name +" </h4> <h4> email:"+ email +" </h4>  <br> <h4> number:"+ number +" </h4><br> <h4> Company :"+ company +" </h4> <h4> Shipping-Address:"+ address +" </h4>  <h4> quantity:"+ quantity +" </h4> </div>   " // html body
-};
- 
-// send mail with defined transport object
-transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        res.send(error);
-        console.log("error")
-    }
-    console.log('Message sent: ' + info.response);
-
-});
-
-
-
-var transporter = nodemailer.createTransport({
-    service:'gmail',
-    auth:{
-        user:'arunsharmamoh@gmail.com',
-        pass:'varunarun181'
-    }
-});
- 
-// setup e-mail data with unicode symbols
-var mailOptions = {
-    from:'arunsharmamoh@gmail.com', // sender address
-    to: email, // list of receivers
-    subject: 'Hello ✔', // Subject line
-    text: 'Hello world 🐴', // plaintext body
-    html: '<b> Welcome to SCRATCHNEST </b>' // html body
-};
- 
-// send mail with defined transport object
-transporter.sendMail(mailOptions, function(error, info){
-    if(error){
-        res.send(error);
-        console.log("error")
-    }
-    console.log('Message sent: ' + info.response);
-
-    res.send("both mail sended");
-});
-
-
-
-
-
-
-
-
-
-
-
-    console.log(name);
-    var newdata=new formData({
-
-        Name:name,
-        Email:email,
-Number:number,
-Company:company,
-Quantity:quantity,
-Address:address
-    });
-
-    newdata.save()
-    .then(doc=>{
-        res.status(201).json({
-            message:"Booking Successfull",
-            results:doc
-        });
-    })
-    .catch(err=>{
-        res.json(err);
-    });
 }
 
+
+
+});
+
+}
 });
 
 
--- index.js
+++ index.js
@@ -1,20 +1,22 @@
 var express=require("express");
-
 var formDataApi=require("./api/formdata.js");
 
 const app = express();
 const path = require('path');
 
 
+
 app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header(
-      "Access-Control-Allow-Headers",
-      "Origin, X-Requested-With, Content-Type, Accept,Authorization"
-    );
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE,OPTIONS');
-    next();
-  });
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept,Authorization"
+  );
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE,OPTIONS');
+  next();
+});
+
+
   
 
  app.use("/api",formDataApi);
@@ -28,12 +30,34 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/', function (req, res) {
-  res.send('Hello World')
+
+  const nodemailer = require('nodemailer');
+  
+  
+let mailTransporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'arunsharmamoh@gmail.com',
+        pass: 'Arun5207@'
+    }
+});
+  
+let mailDetails = {
+    from: '<arunsharmamoh@gmail.com>',
+    to: 'arun0318.cse19@chitkara.edu.in',
+    subject: 'Test mail',
+    text: 'this mail is by arun '
+};
+  
+mailTransporter.sendMail(mailDetails, function(err, data) {
+    if(err) {
+        console.log('Error Occurs');
+    } else {
+        console.log('Email sent successfully');
+   
+      }
+});
 })
-
- 
-
-
 
 
 
