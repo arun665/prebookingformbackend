6cc68eacd1b9fe6999f903ee8ede4e54f1bdf8ff
arun sharma <arunsharmamoh@mgmail.com>
2021-05-02 13:18:59 +0530

    sdfsdf

--- index.js
+++ index.js
@@ -17,25 +17,16 @@ app.use(function(req, res, next) {
   });
   
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept,Authorization"
-  );
-  res.header('Access-Control-Allow-Methods', 'GET,PUT,PATCH,POST,DELETE,OPTIONS');
-  next();
-});
-
-app.get("/",function(req,res){
-  res.send("server started");
-})
  app.use("/api",formDataApi);
+
+
 
  
  const bodyParser = require("body-parser")
  app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+
 app.get('/', function (req, res) {
   res.send('Hello World')
 })
